b27b270e5bd339794db566392c4a4cde9a47352d
abhishek.k <abhishek.k@DSNZ390>
2020-03-24 20:51:07 +1300

    API tests

---
+++ Testing/Stepdefinition/RestAPIHelper.cs
@@ -0,0 +1,43 @@
+﻿using RestSharp;
+using System.IO;
+
+namespace Testing.Stepdefinition
+{
+    public static class RestAPIHelper
+    {
+        public static RestClient rc;
+        public static RestRequest rq;
+        public static string url = "https://api.tmsandbox.co.nz/";
+
+        public static RestClient Url()
+        {
+
+            var url = "https://api.tmsandbox.co.nz/";
+            return rc = new RestClient(url);
+        }
+
+        public static RestRequest parameters(string content, string corr, string reqid, string token, string encoding, string conn)
+        {
+            
+            rc = new RestClient(url);
+            rq = new RestRequest("v1/Categories/6327/Details.json", Method.GET);
+            rq.AddHeader("Content-Type", content);
+            rq.AddParameter(new Parameter("X-Correlation-ID", corr, ParameterType.GetOrPost));
+            rq.AddParameter(new Parameter("X-Request-ID", reqid, ParameterType.GetOrPost));
+            rq.AddParameter(new Parameter("Postman-Token", token, ParameterType.GetOrPost));
+            rq.AddParameter(new Parameter("Content-Encoding", encoding, ParameterType.GetOrPost));
+            rq.AddParameter(new Parameter("Connection", conn, ParameterType.GetOrPost));
+            return rq;
+        }
+
+        public static IRestResponse GetResponse(string content, string corr, string reqid, string token, string encoding, string conn)
+        {
+            StreamWriter writer = new StreamWriter("C:\\Users\\abhishek.k\\source\\repos\\Testing\\Testing\\Output\\output.txt");
+            var response = rc.Execute(rq);
+            writer.Write(response.Content.ToString());
+            writer.Close();
+            return response;
+        }
+        
+    }
+}
---
+++ Testing/Stepdefinition/Stepdefinition.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Testing.Stepdefinition
+{
+    [Binding]
+    public class Stepdefinition
+    {
+
+        [Given(@"I have access to the URL for categories")]
+        public void GivenIHaveAccessToTheURLForCategories()
+        {
+            RestAPIHelper.Url();
+        }
+
+        [When(@"I pass headers (.*), (.*), (.*), (.*), (.*) and (.*)")]
+        public void WhenIPassHeadersAnd(string content, string corr, string reqid, string token, string encoding, string conn)
+        {
+            RestAPIHelper.parameters(content, corr, reqid, token, encoding, conn);
+        }
+
+        [Then(@"I am able to see the category name with headers (.*), (.*), (.*), (.*), (.*) and (.*)")]
+        public void ThenIAmAbleToSeeTheCategoryNameWithHeadersAnd(string content, string corr, string reqid, string token, string encoding, string conn)
+        {
+            var response = RestAPIHelper.GetResponse(content, corr, reqid, token, encoding, conn);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "The response has failed");
+            Assert.That(response.ContentType, Is.EqualTo("application/json"));
+            StreamReader reader = new StreamReader("C:\\Users\\abhishek.k\\source\\repos\\Testing\\Testing\\Output\\output.txt");
+            JObject result = JObject.Parse(reader.ReadToEnd());
+            string value = result["Name"].Value<string>();
+            Assert.AreEqual("Carbon credits", value, "The value is not as expected");
+            reader.Dispose();
+        }
+
+        [Then(@"I am able to see the canrelist status with headers (.*), (.*), (.*), (.*), (.*) and (.*)")]
+        public void ThenIAmAbleToSeeTheCanrelistStatusWithHeadersAnd(string content, string corr, string reqid, string token, string encoding, string conn)
+        {
+            var response = RestAPIHelper.GetResponse(content, corr, reqid, token, encoding, conn);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "The response has failed");
+            Assert.That(response.ContentType, Is.EqualTo("application/json"));
+            StreamReader reader = new StreamReader("C:\\Users\\abhishek.k\\source\\repos\\Testing\\Testing\\Output\\output.txt");
+            JObject result = JObject.Parse(reader.ReadToEnd());
+            string value = result["CanRelist"].Value<string>();
+            Assert.AreEqual("True", value, "The value is not as expected");
+            reader.Dispose();
+        }
+
+        [Then(@"I am able to see the promotions name with headers (.*), (.*), (.*), (.*), (.*) and (.*)")]
+        public void ThenIAmAbleToSeeThePromotionsNameWithHeadersAnd(string content, string corr, string reqid, string token, string encoding, string conn)
+        {
+            var response = RestAPIHelper.GetResponse(content, corr, reqid, token, encoding, conn);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "The response has failed");
+            Assert.That(response.ContentType, Is.EqualTo("application/json"));
+            StreamReader reader = new StreamReader("C:\\Users\\abhishek.k\\source\\repos\\Testing\\Testing\\Output\\output.txt");
+            JObject result = JObject.Parse(reader.ReadToEnd());
+            string value = result["Promotions"][1]["Name"].Value<string>();
+            Assert.AreEqual("Gallery", value, "The value is not as expected");
+            string desc = result["Promotions"][1]["Description"].Value<string>();
+            Assert.IsTrue(desc.Contains("2x larger image"), "The values does not match as expected", desc);
+            reader.Dispose();
+        }
+
+    }
+}
